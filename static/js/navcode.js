973f04a88addbbbad08633fb8e2c74e7ddf66091
Steve Moore <srmoore@gmail.com>
2020-04-28 11:34:04 -0400

    Fix formatting

--- static/js/navcode.js
+++ static/js/navcode.js
@@ -1,5 +1,5 @@
 $(function() {
-    $("section h1, section h2, section h3").each(function(){
+    $("section h2, section h3, section h4").each(function(){
         $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
         $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
         $("nav ul li:first-child a").parent().addClass("active");
